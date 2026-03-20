0aa0dbc3266271418bf43ff1b516d1491335f7d8
sasuniii0 <sasuniwijerathne@gmail.com>
2026-03-20 21:35:40 +0530

    Automation Service Initialized only

--- services/Sensor-Service/src/main/java/com/example/Sensor/Service/SensorServiceApplication.java
+++ services/Sensor-Service/src/main/java/com/example/Sensor/Service/SensorServiceApplication.java
@@ -2,8 +2,14 @@ package com.example.Sensor.Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableFeignClients
+@EnableDiscoveryClient
 public class SensorServiceApplication {
 
 	public static void main(String[] args) {
