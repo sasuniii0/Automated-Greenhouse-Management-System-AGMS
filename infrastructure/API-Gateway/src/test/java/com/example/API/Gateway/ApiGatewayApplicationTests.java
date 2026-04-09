3f45d42fee36fdef3fe04e5c2385398dc265c6d2
sasuniii0 <sasuniwijerathne@gmail.com>
2026-04-10 00:10:43 +0530

    update - update config server

--- infrastructure/API-Gateway/src/test/java/com/example/API/Gateway/ApiGatewayApplicationTests.java
+++ infrastructure/API-Gateway/src/test/java/com/example/API/Gateway/ApiGatewayApplicationTests.java
@@ -1,6 +1,6 @@
 package com.example.API.Gateway;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
